cbfb1002f69f8c8d8d97304e6864f8bc0aec2d40
koraypoyraz <koraypoyraz1@gmail.com>
2022-03-28 15:29:11 +0200

    comm

--- app.py
+++ app.py
@@ -7,10 +7,9 @@ st.set_page_config(page_title='TVNZ Recommender System', layout="wide")
 
 # Initialize router
 
-authenticate.authenticate()
-
-if st.session_state['authentication_status']:
-    app = RouterController()
-    app.add_page(homepage.name, homepage.app)
-    app.add_page(metricpage.name, metricpage.app)
-    app.run()
+# authenticate.authenticate()
+# if st.session_state['authentication_status']:
+app = RouterController()
+app.add_page(homepage.name, homepage.app)
+app.add_page(metricpage.name, metricpage.app)
+app.run()
